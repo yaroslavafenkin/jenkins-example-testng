b84351c286e16377ecc03b4425365e841b6c9459
Darin Pope <darin@planetpope.com>
2024-04-01 13:36:42 -0400

    refactor

---
+++ src/test/java/com/planetpope/testng/BasicTestCase.java
@@ -0,0 +1,33 @@
+package com.planetpope.testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BasicTestCase {
+
+    @Test
+    public void addition() {
+        int expected = 42;
+        int actual = 22 + 20;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void subtraction() {
+        int expected = 42;
+        int actual = 84 - 42;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void isTrue() {
+        boolean value = false;
+        Assert.assertTrue(value);
+    }
+
+    @Test
+    public void isFalse() {
+        boolean value = false;
+        Assert.assertTrue(value);
+    }
+}
